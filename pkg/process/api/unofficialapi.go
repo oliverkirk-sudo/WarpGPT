c5aa306fcc2e2624dc06ead87d4f2c490f7763dd
swy <a609923924@gmail.com>
2023-12-03 21:37:22 +0800

    session

--- pkg/process/api/unofficialapi.go
+++ pkg/process/api/unofficialapi.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"WarpGPT/pkg/process"
+	"WarpGPT/pkg/requestbody"
+)
+
+type UnofficialApiProcess struct {
+	process.Process
+}
+
+func (p *UnofficialApiProcess) SetConversation(conversation requestbody.Conversation) {
+	p.Conversation = conversation
+}
+func (p *UnofficialApiProcess) GetConversation() requestbody.Conversation {
+	return p.Conversation
+}
+func (p *UnofficialApiProcess) ProcessMethod() {
+
+}
