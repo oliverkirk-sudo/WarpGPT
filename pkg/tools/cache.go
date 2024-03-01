3e5267183b9f2b872de9c3228f1db4826f8ac803
life <46099295+clcc2019@users.noreply.github.com>
2024-03-01 11:57:39 +0800

    Update cache.go

--- pkg/tools/cache.go
+++ pkg/tools/cache.go
@@ -3,32 +3,42 @@ package tools
 import (
 	"WarpGPT/pkg/logger"
 	"sync"
+	"time"
 )
 
-var AllCache Cache
-
-type CacheItem interface{}
+type CacheItem struct {
+	Data      interface{}
+	ExpiresAt time.Time
+}
 
 type Cache struct {
 	items map[string]CacheItem
 	lock  sync.Mutex
 }
 
+var AllCache Cache
+
 func init() {
 	AllCache = Cache{items: make(map[string]CacheItem)}
 }
-func (c *Cache) CacheSet(key string, value CacheItem) {
-	c.lock.Lock()
-	logger.Log.Debug("CacheSet")
-	defer c.lock.Unlock()
 
-	c.items[key] = value
+func (c *Cache) CacheSet(key string, value CacheItem, expiration time.Duration) {
+    c.lock.Lock()
+    defer c.lock.Unlock()
+		value.ExpiresAt = time.Now().Add(expiration)
+    c.items[key] = value
+	logger.Log.Debug("CacheSet: Key =", key, "Expiration =", expiration, "Data =", value.Data)
 }
 
 func (c *Cache) CacheGet(key string) (CacheItem, bool) {
 	c.lock.Lock()
-	logger.Log.Debug("CacheGet")
 	defer c.lock.Unlock()
+
 	item, exists := c.items[key]
-	return item, exists
+	if exists && item.ExpiresAt.After(time.Now()) {
+		logger.Log.Debug("CacheGet (Hit): Key =", key, "Expiration =", item.ExpiresAt, "Data =", item.Data)
+		return item, true
+	}
+	logger.Log.Debug("CacheGet (Miss): Key =", key)
+	return CacheItem{}, false
 }
